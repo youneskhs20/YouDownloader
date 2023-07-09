40f3f4d1697ab67ad9b7b21c0ad246cf1f2bf1a8
Younes Khonsari zade <yuneskhs20@gmai.com>
2023-07-09 11:59:45 +0330

    changing github button link in header

--- src/components/TheYouDownloaderHeader.vue
+++ src/components/TheYouDownloaderHeader.vue
@@ -1,11 +1,19 @@
 <template>
   <nav class="w-full h-10rem p-5 flex justify-content-between">
     <div>
-      <img src="../assets/logo.png" class="w-20rem" alt="logo" />
+      <img 
+        src="../assets/logo.png" 
+        alt="logo" 
+        class="w-20rem" 
+      >
     </div>
 
     <a href="https://github.com/youneskhs20/YouDownloader">
-      <Button icon="bi bi-github" severity="secondary" label="GitHub" />
+      <Button 
+        icon="bi bi-github" 
+        severity="secondary" 
+        label="GitHub" 
+      />
     </a>
   </nav>
 </template>
